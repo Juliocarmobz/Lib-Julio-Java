f8c8421b124db3e28389b8e1ddb7be87ba231e89
Julio Carmo <juliocarmobz@gmail.com>
2023-12-18 21:02:09 -0300

    docs: Adicionando documentação

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lib.julio","l":"Calculadora"},{"p":"lib.julio","l":"Conversor"},{"p":"lib.julio","l":"Mat"}];updateSearchResults();
